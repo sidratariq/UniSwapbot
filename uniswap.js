7013f97a75241ab8119085e865c94adf0b80bb0a
sidratariq <sidratariq48@gmail.com>
2022-11-29 02:12:35 +0500

    UniSwap Sniping bot

--- uniswap.js
+++ uniswap.js
@@ -1,17 +1,23 @@
 const ethers = require('ethers');
+const provider = new ethers.providers.WebSocketProvider('wss://eth-goerli.g.alchemy.com/v2/PmsU75Ad9KLTYa-UVfe2inBHo3yXbJSX');
 
+//Address for Router,Factory and WEth
+//We need WETH because pairs
 const addresses = {
-  WETH: '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6',
-  factory: '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f',  //used to create new pairs and we are listening to that event
-  router: '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D',
+  WETH:      '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6',
+  factory:   '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f',  //used to create new pairs and we are listening to that event
+  router:    '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D',
   recipient: '0x518E3a4fe18A6aD1708e6a5efAD533C235FcC783' // address receive token bought
 }
 
-const provider = new ethers.providers.WebSocketProvider('wss://eth-goerli.g.alchemy.com/v2/PmsU75Ad9KLTYa-UVfe2inBHo3yXbJSX');
-// const wallet = ethers.Wallet.fromMnemonic(mnemonic); //signing purpose
-const wallet = new ethers.Wallet('0xb15773022ebe1a1f40db803e6aa9afa4da8a1c76a0a0eb2344b21c2f98269e6f',provider); //signing purpose
+const wallet = new ethers.Wallet('your private key here',provider); //signing purpose
 
-console.log("Wallet created::")
+// We buy for 0.1 ETH of the new token 
+const ethAmount = '0.0001';
+const amountIn = ethers.utils.parseUnits(ethAmount, 'ether');
+
+console.log("Wallet created::");
+console.log("Eth value we will buy from",amountIn);
 
 const account = wallet.connect(provider); 
 
@@ -39,12 +45,9 @@ factory.on('PairCreated', async (token0, token1, pairAddress) => {
     pairAddress: ${pairAddress}
   `);
 
-  console.log("Value of pair created",pairAddress)
-
   //The quote currency needs to be WETH (we will pay with WETH)
   //token0 is guaranteed to be strictly less than token1 by sort order.
-
-  let tokenIn, tokenOut;
+ let tokenIn, tokenOut;
   if(token0 === addresses.WETH) {
     tokenIn = token0; 
     tokenOut = token1;
@@ -60,41 +63,36 @@ factory.on('PairCreated', async (token0, token1, pairAddress) => {
     return;
   }
 
-  // 0.01 / 10 
+   // Ideally you'll probably want to take a closer look at reserves, and price from the pair address
+   const uPair = new ethers.Contract(
+    pairAddress,
+    ['function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast)'],
+    account);
 
-  ////We buy for 0.1 ETH of the new token 
-  const amountIn = ethers.utils.parseUnits('0.0001', 'ether');
+  const reserves = await uPair.getReserves();
+
+   // if insufficient liquidity move on
+   if (reserves[0] == 0 && reserves[1] == 0) {
+      console.log(`Token has no liquidity...`);
+    return
+    }
+
   const amounts = await router.getAmountsOut(amountIn, [tokenIn, tokenOut]); //tell you how many tokens you will have as output 
 
-//   //Our execution price will be a bit different, we need some flexbility
-  const amountOutMin = amounts[1].sub(amounts[1].div(10));
-  console.log(`
+   //Our execution price will be a bit different, we need some flexbility
+   const amountOutMin = amounts[1].sub(amounts[1].div(10));
+
+   console.log(`
     Buying new token
     =================
     tokenIn: ${amountIn.toString()} ${tokenIn} (WETH)
     tokenOut: ${amountOutMin.toString()} ${tokenOut}
   `);
 
-    // Ideally you'll probably want to take a closer look at reserves, and price from the pair address
-
-        // const uPair = new ethers.Contract(
-        //     pairAddress,
-        //     ['function getReserves(address factory, address tokenA, address tokenB) internal view returns (uint reserveA, uint reserveB)'],
-        //     account
-        //   );
-        
-        // const reserves = await uPair.getReserves()
-        // // if insufficient liquidity move on
-        
-        // if (reserves[0] == 0 && reserves[1] == 0) {
-        //     console.log(`Token has no liquidity...`)
-        //     return
-        // }
-
 //   add token address check if our token else don't work
 //   determine if you want to snipe this particular token...
 
-if (tokenIn == '0x7Ae21b9C230B4e2916E5cacE81AaE04FF10275a8' || tokenOut =='0x7Ae21b9C230B4e2916E5cacE81AaE04FF10275a8' )
+  if (tokenIn == '0x18f95BF2f84090E2cC54aCdAfFC5DaE1163FEA60' || tokenOut =='0x18f95BF2f84090E2cC54aCdAfFC5DaE1163FEA60' )
  { const tx = await router.swapExactTokensForTokens(
     amountIn, //as input wrap eth
     amountOutMin, // min amount of token you will accept as output 
